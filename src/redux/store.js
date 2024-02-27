2f8bb5ed6858257364dd531fce5b2f95bbf487ae
devdamirbek@gmail.com <developerdamirbek@mail.ru>
2024-02-27 13:36:25 +0500

    localStorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,19 @@
+// store.js
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './reducers/cart-reducer'; 
+import cartReducer from './reducers/cart-reducer';
+import { loadState, saveState } from '../lib/storage';
 
-export default configureStore({
+const persistedState = loadState('reduxState');
+
+const store = configureStore({
   reducer: {
-    cart: cartReducer, 
+    cart: cartReducer,
   },
+  preloadedState: persistedState,
 });
+
+store.subscribe(() => {
+  saveState('reduxState', store.getState());
+});
+
+export default store;
